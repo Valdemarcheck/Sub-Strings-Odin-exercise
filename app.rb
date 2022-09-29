869c4da867a1d1bef2e68dff6eb9a9c0cb43f877
Valdemarcheck <valekzhaninvladimir58@gmail.com>
2022-09-29 18:32:33 +0500

    Setup an algorithm to count occurrences of substrings in a given string

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-
+    words = string.downcase.split
+    substring_occurrences = Hash.new(0)
+    words.each do |word|
+        dictionary.each do |dict_word|
+            if word.include?(dict_word)
+                occurrences = word.count(dict_word)
+                substring_occurrences[dict_word] += 1
+            end
+        end
+    end
+    substring_occurrences
 end
